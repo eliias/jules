94a66afb2108bd8576a7f5b6ddb92defb2744138
Hannes Moser <hannes@impossiblearts.com>
2015-02-21 11:15:11 +0100

    readded assets

---
+++ assets/sprites/Terrain Objects/Platform_Jungle/tmx/Tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset1" tilewidth="64" tileheight="64">
+ <image source="Tileset1.png" width="576" height="448"/>
+</tileset>
---
+++ assets/sprites/Terrain Objects/Platform_Jungle/tmx/Tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset3" tilewidth="64" tileheight="64">
+ <image source="Tileset3.png" width="448" height="256"/>
+</tileset>
